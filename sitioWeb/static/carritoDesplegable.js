f6ad239cc3eed8c97f50bcdada014f62cc9535d4
ruben <rubenlazartev2011@gmail.com>
2024-11-20 16:10:58 -0400

    arreglando bug boton comprar del carrito que no aparecia

--- sitioWeb/static/carritoDesplegable.js
+++ sitioWeb/static/carritoDesplegable.js
@@ -145,6 +145,8 @@ function actualizarPopup(productos) {
             });
         });
     }
+    cartPopup.innerHTML += `<a href="/transaccion/" class="btn-buy">Comprar</a>`;
+
 }
 // Función para obtener el token CSRF
 function getCookie(name) {
