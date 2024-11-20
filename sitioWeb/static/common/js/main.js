b91e0e910aa173a7d5c8becc55cb1c99e33a19e7
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2024-11-20 18:37:24 -0400

    Añadiendo Funcionalidad al boton de cancelar, y mensaje de advertencia antes de suspender una cuenta de usuario

--- sitioWeb/static/common/js/main.js
+++ sitioWeb/static/common/js/main.js
@@ -1,4 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkbox = document.getElementById('id_estadoUsuario');  // Obtén el checkbox por su id
 
+    checkbox.addEventListener('change', function(event) {
+        // Verificar si el checkbox está marcado antes de mostrar la alerta
+        if (!checkbox.checked) {
+            // Mostrar la alerta clásica de confirmación cuando se cambie el estado
+            var confirmSuspension = window.confirm("¿Estás seguro de suspender a este usuario?");
+            
+            if (!confirmSuspension) {
+                // Si el usuario cancela, revertir el estado del checkbox
+                checkbox.checked = !checkbox.checked;
+            }
+        }
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     // Crear el botón de "Cancelar"
@@ -15,6 +30,18 @@ document.addEventListener('DOMContentLoaded', function() {
         actionContainer.appendChild(div);
     }
 
-  
+    // Función para resetear el formulario y redirigir
+    button.addEventListener("click", function(event) {
+        event.preventDefault();  // Prevenir el comportamiento predeterminado del botón (por ejemplo, enviar el formulario)
+
+        // Resetear el formulario
+        var form = document.querySelector('form');
+        if (form) {
+            form.reset();  // Resetea todos los campos del formulario
+        }
+
+        // Redirigir a la página de lista de usuarios
+        window.location.href = '/admin/sitioWeb/usuario/';  // Redirige a la lista de usuarios
+    });
 });
 
